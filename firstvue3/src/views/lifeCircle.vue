aebb4a636af8e97384bd250ff2caf3d6159514a1
LsJoker <30564101+LsJoker@users.noreply.github.com>
2021-01-10 22:04:38 +0800

    对比 Signed-off-by: LsJoker <30564101+LsJoker@users.noreply.github.com>

--- firstvue3/src/views/lifeCircle.vue
+++ firstvue3/src/views/lifeCircle.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <ul>
         <li>
-            vue2.x生命周期回顾
+            <h3>vue2.x生命周期回顾</h3>
             <ol>
                 <li><code>beforeCreate</code>，在实例初始化之后，数据观测 (data observer) 和 event/watcher 事件配置之前被调用。</li>
                 <li><code>created</code>，在实例创建完成后被立即调用。在这一步，实例已完成以下的配置：数据观测 (data observer)，属性和方法的运算，watch/event 事件回调。然而，挂载阶段还没开始，$el 属性目前尚不可用。</li>
@@ -16,7 +16,23 @@
                 <li><code>destroyed</code>，实例销毁后调用。该钩子被调用后，对应 Vue 实例的所有指令都被解绑，所有的事件监听器被移除，所有的子实例也都被销毁。</li>           
                 <li><code>errorCaptured</code>，当捕获一个来自子孙组件的错误时被调用。</li>           
             </ol>
-
+        </li>
+        <li>
+            <h3>vue3.0生命周期使用</h3>
+            <ol>
+                <li>
+                    3.0与2.0生命周期对比：<br>
+                    beforeCreate -> setup()<br>
+                    created -> setup()<br>
+                    beforeMount -> onBeforeMount<br>
+                    mounted -> onMounted<br>
+                    beforeUpdate -> onBeforeUpdate<br>
+                    updated -> onUpdated<br>
+                    beforeDestroy -> onBeforeUnmount<br>
+                    destroyed -> onUnmounted<br>
+                    errorCaptured -> onErrorCaptured<br>
+                </li>            
+            </ol>
         </li>
     </ul>
   </div>
